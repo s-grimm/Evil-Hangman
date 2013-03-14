09ce1028dc30a8da64202135d14627896bee241e
Shane Grimminck <shane.grimminck@gmail.com>
2013-03-14 10:43:07 -0400

    Added Content from Local Project

---
+++ EvilHangmanLibrary/Evil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace EvilHangmanLibrary
+{
+    public static class Evil
+    {
+        public static List<string> GetWordsForLength(int Length)
+        {
+            List<string> returnList = new List<string>();
+            try
+            {
+                using (StreamReader reader = new StreamReader(File.OpenRead(@"res\dictionary.txt")))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        if (line.Length == Length)
+                        {
+                            returnList.Add(line);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) { }
+            return returnList;
+        }
+    }
+}
